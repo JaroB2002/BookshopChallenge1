19560c93cceaa87a118742e7ec7c2fff2e918985
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2022-11-09 19:50:33 +0100

    Add files via upload
    Upload 1 (via niet terminal)

---
+++ App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import CourseTile from './components/CourseTile';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading1}>Courses</Text>
+      <View style={styles.tilesContainer}>
+
+        <CourseTile title="Book title 1" subtitle="by Some Random dude" />
+        <CourseTile title="Book title 2" subtitle="by some other dude"/>
+        <CourseTile title="Book title 3" subtitle="by Jane Doe"/>
+        <CourseTile title="Book title 4" subtitle="By John Doe"/>
+
+      <StatusBar style="auto" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 42,
+  },
+  heading1:{
+    fontSize: 26,
+    margin: 0,
+    marginLeft: 15,
+  },
+  tilesContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    display:"flex",
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+# BookshopChallenge1
